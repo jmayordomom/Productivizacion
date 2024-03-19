e46b20014087e9de212d6c9bb1d7279e9f1f6652
jmayordomom <jaimemayordomomoreno@gmail.com>
2024-03-19 22:51:45 +0100

    jhgfds

--- titanic.py
+++ titanic.py
@@ -7,21 +7,16 @@ import numpy as np
 # import seaborn as sns
 # import sklearn
 # Modelos
-from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder, MinMaxScaler
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split,cross_validate
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier, BaggingClassifier
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, f1_score, precision_score, recall_score, \
-roc_curve, roc_auc_score, ConfusionMatrixDisplay, multilabel_confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 import streamlit as st
-import os
 
 #Funciones
 def clasificar_nombre(nombre):
